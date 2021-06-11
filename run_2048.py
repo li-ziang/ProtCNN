bf353253f38b320f85ba5f87a2b0b01e1ba9ac61
liziang <983417395@qq.com>
2021-06-11 18:41:20 +0800

    update

--- run_2048.py
+++ run_2048.py
@@ -29,7 +29,7 @@ from tensorflow.keras.layers import Embedding, Bidirectional,LSTM, GlobalMaxPool
 from tensorflow.keras.mixed_precision import experimental as mixed_precision
 import math
 
-def residual_block(data, filters, d_rate,layer_index, bottleneck_factor=0.5, first_dilated_layer=2):
+def residual_block(data, filters, d_rate,layer_index, bottleneck_factor=0.5, first_dilated_layer=2,training=False):
     shifted_layer_index = layer_index - first_dilated_layer + 1
     dilation_rate = max(1, d_rate**shifted_layer_index)
     num_bottleneck_units = math.floor(
@@ -38,12 +38,12 @@ def residual_block(data, filters, d_rate,layer_index, bottleneck_factor=0.5, fir
 
     shortcut = data
 
-    x = BatchNormalization()(data)
+    x = BatchNormalization()(data,training=training)
     x = Activation('relu')(x)
     x = Conv1D(num_bottleneck_units, kernel_size, dilation_rate=dilation_rate, padding='same', kernel_regularizer=l2(0.001))(x)
 
     #bottleneck convolution
-    x = BatchNormalization()(x)
+    x = BatchNormalization()(x,training=training)
     x = Activation('relu')(x)
 
     x = Conv1D(filters, 1, dilation_rate=1, padding='same', kernel_regularizer=l2(0.001))(x)
@@ -71,7 +71,7 @@ with strategy.scope():
     # per-residue representation
     # res = residual_block(conv, filters, 1)
     for i in range(5):
-        x = residual_block(x, filters, 3,i)
+        x = residual_block(x, filters, 3,i,training=True)
 
     # res = Conv1D(128, 1, padding='same', kernel_regularizer=l2(0.001))(res)
     # print(res5.shape)
@@ -89,7 +89,7 @@ with strategy.scope():
 
 # lr_schedule according to paper
 
-    # initial_learning_rate = 0.0001
+    initial_learning_rate = 0.0001
     # lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
     #     initial_learning_rate,
     #     decay_steps=1000,
@@ -99,7 +99,7 @@ with strategy.scope():
     # model2.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=lr_schedule),
     #                 loss='categorical_crossentropy',
     #                 metrics=['accuracy'])
-    model2.compile(optimizer=tf.keras.optimizers.Adam(),
+    model2.compile(optimizer=tf.keras.optimizers.Adam(initial_learning_rate),
                     loss='categorical_crossentropy',
                     metrics=['accuracy'])
     model2.summary()
@@ -126,7 +126,7 @@ y_val_sp= sparse.load_npz('y_val_2048.npz')
 val_y_b=y_val_sp.toarray()
 
 
-batch_size=64
+batch_size=32
 checkpoint_filepath="model_brand_new"
 
 model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
@@ -140,7 +140,7 @@ val_dataset=tf.data.Dataset.from_tensor_slices((val_ohe_b, val_y_b)).batch(batch
 
 
 es = EarlyStopping(monitor='val_loss', patience=3, verbose=1)
-history2=model2.fit(train_dataset, epochs=30, validation_data=val_dataset,callbacks=[es,model_checkpoint_callback])
+history2=model2.fit(train_dataset, epochs=30, validation_data=val_dataset,callbacks=[model_checkpoint_callback])
 
 model2.save_weights('model_2048_all.h5')
 
