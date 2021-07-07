98f67a009db33d53e1cf8283322f5a2b4aab7f48
liziang <983417395@qq.com>
2021-07-07 23:27:33 +0800

    98.6%

---
+++ NewFile.md
@@ -0,0 +1,20 @@
+长度1024跑不起来，需要处理加起来一共100G的矩阵，在model.fit步骤中killed掉了，因为使用的数据占用了较多的内存，在达到70%的时候killed了。
+
+
+
+100的只有40%
+
+
+
+100,17929 95.16%
+
+1024 40%，17929 85%
+
+
+
+![image-20210531184551935](/Users/liziang/Library/Application Support/typora-user-images/image-20210531184551935.png)
+
+88%
+
+
+
--- run_2048.py
+++ run_2048.py
@@ -12,9 +12,11 @@ The labels are one-hot tensors in length 17929, science there are 17929 classes 
 
 '''
 
+import numpy as np
+from scipy import sparse
 import os
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,4,5,6,7"
+os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3,4,5,6,7"
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.regularizers import l2
@@ -28,6 +30,53 @@ from tensorflow.keras.layers import Conv1D, Add, MaxPooling1D, BatchNormalizatio
 from tensorflow.keras.layers import Embedding, Bidirectional,LSTM, GlobalMaxPooling1D
 from tensorflow.keras.mixed_precision import experimental as mixed_precision
 import math
+from tensorflow.keras.callbacks import Callback
+import tensorflow.keras.backend as K
+import tensorflow as tf
+
+
+class WarmUp(tf.keras.optimizers.schedules.LearningRateSchedule):
+
+    def __init__(
+        self,
+        initial_learning_rate: float,
+        decay_schedule_fn,
+        warmup_steps: int,
+        name: str = None,
+    ):
+        super().__init__()
+        self.initial_learning_rate = initial_learning_rate
+        self.warmup_steps = warmup_steps
+        self.decay_schedule_fn = decay_schedule_fn
+        self.name = name
+
+    def __call__(self, step):
+        with tf.name_scope(self.name or "WarmUp") as name:
+            # Implements polynomial warmup. i.e., if global_step < warmup_steps, the
+            # learning rate will be `global_step/num_warmup_steps * init_lr`.
+
+            global_step_float = tf.cast(step, tf.float32)
+            warmup_steps_float = tf.cast(self.warmup_steps, tf.float32)
+            warmup_percent_done = global_step_float / warmup_steps_float
+            warmup_learning_rate = self.initial_learning_rate * warmup_percent_done
+            learning_rate = tf.cond(
+                global_step_float < warmup_steps_float,
+                lambda: warmup_learning_rate,
+                lambda: self.decay_schedule_fn(step - self.warmup_steps),
+                name=name,
+            )
+            return learning_rate
+
+    def get_config(self):
+        return {
+            "initial_learning_rate": self.initial_learning_rate,
+            "decay_schedule_fn": self.decay_schedule_fn,
+            "warmup_steps": self.warmup_steps,
+            "power": self.power,
+            "name": self.name,
+        }
+
+
 
 def residual_block(data, filters, d_rate,layer_index, bottleneck_factor=0.5, first_dilated_layer=2,training=False):
     shifted_layer_index = layer_index - first_dilated_layer + 1
@@ -37,23 +86,18 @@ def residual_block(data, filters, d_rate,layer_index, bottleneck_factor=0.5, fir
     kernel_size = 21
 
     shortcut = data
-
     x = BatchNormalization()(data,training=training)
     x = Activation('relu')(x)
     x = Conv1D(num_bottleneck_units, kernel_size, dilation_rate=dilation_rate, padding='same', kernel_regularizer=l2(0.001))(x)
-
-    #bottleneck convolution
     x = BatchNormalization()(x,training=training)
     x = Activation('relu')(x)
-
     x = Conv1D(filters, 1, dilation_rate=1, padding='same', kernel_regularizer=l2(0.001))(x)
-
-    #skip connection
     x = Add()([x, shortcut])
-
     return x
 
 strategy = tf.distribute.MirroredStrategy()
+import tensorboard
+import datetime
 
 # # Open a strategy scope.
 with strategy.scope():
@@ -68,49 +112,37 @@ with strategy.scope():
     kernel_size = 21
     x = Conv1D(filters, kernel_size, padding='same')(x_input) 
 
-    # per-residue representation
-    # res = residual_block(conv, filters, 1)
     for i in range(5):
         x = residual_block(x, filters, 3,i,training=True)
 
-    # res = Conv1D(128, 1, padding='same', kernel_regularizer=l2(0.001))(res)
-    # print(res5.shape)
     x = GlobalMaxPooling1D()(x)
-    # x = Dropout(0.5)(x)
-    # x = Conv1D(128, 1, padding='same')(x) 
 
-
-    # softmax classifier
-    # x = Flatten()(x)
     x_output = Dense(17929, kernel_regularizer=l2(0.0001))(x)
     x_output = Activation('softmax', dtype='float32', name='predictions')(x_output)
 
     model2 = Model(inputs=x_input, outputs=x_output)
 
-# lr_schedule according to paper
+    log_dir="logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    train_summary_writer = tf.summary.create_file_writer(log_dir)
+
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
     initial_learning_rate = 0.0001
-    # lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-    #     initial_learning_rate,
-    #     decay_steps=1000,
-    #     decay_rate=0.997,
-    #     staircase=True)
-
+    lr_func = tf.keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate,
+        decay_steps=1000,
+        decay_rate=0.997,
+        staircase=True)
+    lr_schedule = WarmUp(initial_learning_rate,lr_func,1000)
     # model2.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=lr_schedule),
     #                 loss='categorical_crossentropy',
     #                 metrics=['accuracy'])
-    model2.compile(optimizer=tf.keras.optimizers.Adam(initial_learning_rate),
+    model2.compile(optimizer=tf.keras.optimizers.Adam(lr_schedule),
                     loss='categorical_crossentropy',
                     metrics=['accuracy'])
     model2.summary()
 
-# import tensorboard
-# log_dir="./logs/"
-# tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1) 
-
-import numpy as np
-from scipy import sparse
-
+ 
 train_ohe_sp= sparse.load_npz('train_ohe_2048.npz')
 train_ohe_s=train_ohe_sp.toarray()
 train_ohe_b=np.reshape(train_ohe_s, (1086741, 2048, 21))
@@ -124,7 +156,6 @@ val_ohe_b=np.reshape(val_ohe_s, (126171, 2048, 21))
 
 y_val_sp= sparse.load_npz('y_val_2048.npz')
 val_y_b=y_val_sp.toarray()
-
 
 batch_size=32
 checkpoint_filepath="model_brand_new"
@@ -140,38 +171,6 @@ val_dataset=tf.data.Dataset.from_tensor_slices((val_ohe_b, val_y_b)).batch(batch
 
 
 es = EarlyStopping(monitor='val_loss', patience=3, verbose=1)
-history2=model2.fit(train_dataset, epochs=30, validation_data=val_dataset,callbacks=[model_checkpoint_callback])
+history2=model2.fit(train_dataset, epochs=30, validation_data=val_dataset,callbacks=[model_checkpoint_callback,tensorboard_callback])
 
 model2.save_weights('model_2048_all.h5')
-
-
-
-
-# def residual_block(data, filters, d_rate):
-#   """
-#   _data: input
-#   _filters: convolution filters
-#   _d_rate: dilation rate
-#   """
-
-#     shortcut = data
-
-#     bn1 = BatchNormalization()(data)
-#     act1 = Activation('relu')(bn1)
-#     conv1 = Conv1D(filters, 21, dilation_rate=d_rate, padding='same', kernel_regularizer=l2(0.001))(act1)
-
-#     #bottleneck convolution
-#     bn2 = BatchNormalization()(conv1)
-#     act2 = Activation('relu')(bn2)
-#     conv2 = Conv1D(filters*2, 1, padding='same', kernel_regularizer=l2(0.001))(act2)
-#     bn3 = BatchNormalization()(conv2)
-#     act3 = Activation('relu')(bn3)
-#     conv3 = Conv1D(filters*2, 21, padding='same', kernel_regularizer=l2(0.001))(act3)
-#     bn4 = BatchNormalization()(conv3)
-#     act4 = Activation('relu')(bn4)
-#     conv4 = Conv1D(filters, 1, padding='same', kernel_regularizer=l2(0.001))(act4)
-
-#     #skip connection
-#     x = Add()([conv4, shortcut])
-
-#     return x
